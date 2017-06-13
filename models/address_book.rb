5cb372efd7bf7ec2c6bc1fc22ae8d6b7203d212d
Thom <tphtiger@yahoo.com>
2017-06-13 11:43:11 -0400

    added both binary_search and iterative_search methods with rspec tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -39,4 +39,42 @@ class AddressBook
       end
     end
   end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    # #5
+    return nil
+  end
+
+  def iterative_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower < upper
+      if name == entries.name
+        return entries.name
+      end
+    end
+
+    return nil
+
+  end
 end
